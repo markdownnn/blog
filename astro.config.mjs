8b7ff10c498ddac86126deddfbdf5a653fbfc097
markdownnn <59331354+markdownnn@users.noreply.github.com>
2026-07-01 12:24:04 +0900

    chore: deploy to Cloudflare Workers at blog.teamnyongs.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mark-blog.vercel.app',
+  site: 'https://blog.teamnyongs.com',
 
   vite: {
     plugins: [tailwindcss()]
